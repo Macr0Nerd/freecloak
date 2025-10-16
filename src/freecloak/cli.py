507475b0264c6e24882b6ee6dfd41ca8d4285abb
Gabriele A. Ron <developer@groncyber.com>
2025-10-16 02:18:08 -0500

    Initial commit

---
+++ src/freecloak/cli.py
@@ -0,0 +1,92 @@
+##############################################################################
+##  Copyright (C) 2025  Gabriele Ron                                        ##
+##                                                                          ##
+##  This program is free software: you can redistribute it and/or modify    ##
+##  it under the terms of the GNU General Public License as published by    ##
+##  the Free Software Foundation, either version 3 of the License, or       ##
+##  (at your option) any later version.                                     ##
+##                                                                          ##
+##  This program is distributed in the hope that it will be useful,         ##
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+##  GNU General Public License for more details.                            ##
+##                                                                          ##
+##  You should have received a copy of the GNU General Public License       ##
+##  along with this program.  If not, see <https://www.gnu.org/licenses/>.  ##
+##############################################################################
+
+
+import argparse
+import importlib
+import logging
+
+from freecloak import __version__
+from freecloak.abstract import PluginInfo
+from freecloak.app import discover_plugins
+from freecloak.logging import configure_logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def add_connection_arguments(parser: argparse.ArgumentParser) -> None:
+    keycloak_group = parser.add_argument_group('keycloak')
+
+def add_miscellaneous_arguments(parser: argparse.ArgumentParser) -> None:
+    miscellaneous_group = parser.add_argument_group('miscellaneous')
+
+    miscellaneous_group.add_argument('-h', '--help', action='help')
+    miscellaneous_group.add_argument('-V', '--version', action='version', version=f'freecloak v{__version__}')
+
+def add_logging_arguments(parser: argparse.ArgumentParser) -> None:
+    console_logging_group = parser.add_argument_group('console logging options')
+
+    console_logging_group_me = console_logging_group.add_mutually_exclusive_group()
+    console_logging_group_me.add_argument('-v', '--verbose', help='increase console log level', action='count')
+    console_logging_group_me.add_argument('-q', '--quiet', help='decrease console log level', action='count')
+
+    console_logging_group.add_argument('--suppress-stdout', action='store_true')
+
+
+    file_logging_group = parser.add_argument_group('file logging options')
+    file_logging_group.add_argument('--output-log-file', help='main log file to use', metavar='FILE')
+    file_logging_group.add_argument('--output-log-level', help='file logging level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+
+def main() -> int:
+    root_parser = argparse.ArgumentParser(
+        prog="freecloak",
+        description="FreeIPA and Keycloak integration tool",
+        epilog="Built & Developed by Gabriele Ron (developer@groncyber.com)",
+        add_help=False,
+    )
+
+    # Configure logging first so we can use it when discovering plugins if necessary
+    add_logging_arguments(root_parser)
+    logging_args, cli_args = root_parser.parse_known_args()
+    configure_logging(**vars(logging_args))
+
+    plugins = discover_plugins()
+
+    global_arg_funcs = [add_logging_arguments, add_miscellaneous_arguments]
+    subparsers = root_parser.add_subparsers(help='plugin help', dest='plugin', metavar='PLUGIN', required=True)
+    for plugin_name, plugin_module in plugins.items():
+        plugin_info: PluginInfo = plugin_module.__plugin_info__
+
+        plugin_parser = subparsers.add_parser(plugin_info.plugin_name, help=f'{plugin_info.plugin_name} help', add_help=False)
+        plugin_subparsers = plugin_parser.add_subparsers(help='command help', dest='command', metavar='COMMAND', required=True)
+
+        plugin_app_module = importlib.import_module(f'{plugin_name}.app')
+        plugin_app_module.add_plugin_parser(plugin_subparsers, global_arg_funcs=global_arg_funcs)
+
+        for global_arg_func in global_arg_funcs:
+            global_arg_func(plugin_parser)
+
+    add_miscellaneous_arguments(root_parser)
+
+    args = vars(root_parser.parse_args())
+
+    try:
+        return getattr(plugins[f"freecloak.plugins.{args['plugin']}"], args['command'])(**args)
+    except AttributeError:
+        logger.error(f'Plugin "{args["plugin"]}" has no method "{args["command"]}"')
+        return 2
---
+++ src/freecloak/logging/utils.py
@@ -0,0 +1,101 @@
+##############################################################################
+##  Copyright (C) 2025  Gabriele Ron                                        ##
+##                                                                          ##
+##  This program is free software: you can redistribute it and/or modify    ##
+##  it under the terms of the GNU General Public License as published by    ##
+##  the Free Software Foundation, either version 3 of the License, or       ##
+##  (at your option) any later version.                                     ##
+##                                                                          ##
+##  This program is distributed in the hope that it will be useful,         ##
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of          ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           ##
+##  GNU General Public License for more details.                            ##
+##                                                                          ##
+##  You should have received a copy of the GNU General Public License       ##
+##  along with this program.  If not, see <https://www.gnu.org/licenses/>.  ##
+##############################################################################
+
+
+import logging
+import logging.config
+from typing import Optional
+
+from freecloak.logging.filters import filters
+
+
+def configure_logging(
+    *,
+    output_log_file: Optional[str] = None,
+    output_log_level: Optional[str | int] = None,
+    quiet: Optional[int] = None,
+    verbose: Optional[int] = None,
+    suppress_stdout: Optional[bool] = None,
+    **_
+) -> None:
+    if output_log_level is None:
+        output_log_level = logging.INFO
+    elif isinstance(output_log_level, str):
+        output_log_level = getattr(logging, output_log_level.upper())
+
+    if quiet and verbose:
+        raise ValueError('the quiet and verbose options are mutually exclusive')
+
+    console_log_level = logging.WARNING
+
+    if quiet:
+        console_log_level += min(quiet * 10, 20)
+    elif verbose:
+        console_log_level -= min(verbose * 10, 20)
+
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console': {
+                'format': '({asctime}) {name} [{levelname}]: {message}',
+                'datefmt': '%H:%M:%S',
+                'style': '{',
+                'validate': True
+            },
+            'file': {
+                'format': '{asctime} {name} {levelname}: {message}',
+                'datefmt': '%Y-%m-%d %H:%M:%S',
+                'style': '{',
+                'validate': True
+            },
+        },
+        'handlers': {
+            'stdout': {
+                'class': 'logging.StreamHandler',
+                'level': console_log_level,
+                'formatter': 'console',
+                'stream': 'ext://sys.stdout',
+                'filters': [filters['level_below'][logging.WARNING]],
+            },
+            'stderr': {
+                'class': 'logging.StreamHandler',
+                'level': logging.ERROR,
+                'formatter': 'console',
+                'stream': 'ext://sys.stderr',
+            },
+        },
+        'root': {
+            'level': logging.DEBUG,
+            'handlers': ['stdout', 'stderr'],
+        }
+    }
+
+    if suppress_stdout:
+        logging_config['root']['handlers'].remove('stdout')
+
+    if output_log_file:
+        logging_config['handlers']['file'] = {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': output_log_level,
+            'formatter': 'file',
+            'filename': output_log_file,
+        }
+
+        logging_config['root']['handlers'].append('file')
+
+    logging.config.dictConfig(logging_config)
